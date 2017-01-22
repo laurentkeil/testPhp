93861391fa299ba4535a9ef76724bb7e9f743da4
laurentkeil <laurent_0306@hotmail.com>
2017-01-22 17:57:03 +0100

    php app pure

---
+++ Membre.class.php
@@ -0,0 +1,52 @@
+﻿<?php
+class Membre
+{
+    private $pseudo;
+    private $email;
+    private $signature;
+    private $actif;
+    
+	public function __construct()
+    {
+    	// Ex : Récupérer en base de données les infos du membre
+    	// SELECT pseudo, email, signature, actif FROM membres WHERE id = ...
+    	
+    }
+	
+	
+	public function __destruct()
+	{
+		echo '</br>Cet objet va être détruit !';
+	}
+	
+    public function envoyerEMail($titre, $message)
+    {
+        //mail($this->email, $titre, $message);
+    }
+    
+    public function bannir()
+    {
+        $this->actif = false;
+        $this->envoyerEMail('Vous avez été banni', 'Ne revenez plus !');
+    }
+	
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+    
+    public function setPseudo($nouveauPseudo)
+    {
+			// Vérifier si le nouveau pseudo n'est ni vide ni trop long
+		if (!empty($nouveauPseudo) AND strlen($nouveauPseudo) < 15)
+		{
+			// Ok, on change son pseudo
+			$this->pseudo = $nouveauPseudo;
+		}
+    }
+	
+	public function setEmail($nouveauMail){
+		$this->email = $nouveauMail;
+	}
+}
+?>
---
+++ cible.php
@@ -0,0 +1,25 @@
+﻿<p>Bonjour !</p>
+
+<p>Je sais comment tu t'appelles, hé hé. Tu t'appelles <?php echo htmlspecialchars($_POST['prenom']); ?> !</p>
+
+<p>Si tu veux changer de prénom, <a href="form.php">clique ici</a> pour revenir à la page du formulaire.</p>
+
+<form action="cible_envoi.php" method="post" enctype="multipart/form-data">
+        <p>
+                Formulaire d'envoi de fichier :<br />
+                <input type="file" name="monfichier" /><br />
+                <input type="submit" value="Envoyer le fichier" />
+        </p>
+</form>
+
+<?php
+
+include_once('Membre.class.php');
+
+$membre = new Membre();
+$membre->setPseudo('M@teo21');
+$membre->setEmail('laurent_0306@hotmail.com');
+echo $membre->getPseudo() . ', je vais te bannir !';
+$membre->bannir();
+
+?>
